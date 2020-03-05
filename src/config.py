22a394a1ad6ec8b9ae618a67bba995d6aa79003f
Charles Reid <chmreid@ucsc.edu>
2020-03-05 02:08:33 -0800

    switch to singleton pattern for config, use external file

--- src/config.py
+++ src/config.py
@@ -8,17 +8,57 @@ class FooConfig(Enum):
     BAR = auto()
 
 
-class Config:
-    _FOO_VAR: typing.Optional[str] = None
+class Config(object):
+    """
+    Singleton class to allow uniform access to centillion configuration.
+    Uses a singleton pattern to store the config file location once.
+    
+    Example:
+        
+        >>> c = Config('/path/to/my/config.json')
+        >>> print(Config.get_config_file())
+        /path/to/my/config.json
+    """
+
+    #########################
+    # Begin Singleton Section
+    #########################
+
+    _CONFIG_FILE: typing.Optional[str] = None
+    _CONFIG: typing.Optional[dict] = None
+
+    def __init__(self, config_file = None):
+        if config_file = None:
+            config_file = Config.get_required_env_var("CENTILLION_CONFIG")
+
+        # Check that specified config file exists
+        assert os.path.exists(config_file)
+
+        # Use singleton pattern to store config file location/load config once
+        if not Config._CONFIG:
+            Config._CONFIG_FILE = config_file
+            with open(config_file, 'r') as f:
+                Config._CONFIG = json.load(f)
 
     @staticmethod
-    def _get_required_envvar(envvar: str) -> str:
+    def get_required_env_var(envvar: str) -> str:
         if envvar not in os.environ:
-            raise Exception("Please set the {} environment variable".format(envvar))
+            raise Exception("Please set the {envvar} environment variable")
         return os.environ[envvar]
-    
+
     @staticmethod
-    def get_foo_var() -> str:
-        val = Config._get_required_envvar("FOO_VAR")
-        Config._FOO_VAR = val
-        return Config._FOO_VAR
+    def get_config_file() -> str:
+        return Config._CONFIG_FILE.config_file
+
+    #############################
+    # Begin Configuration Section
+    #############################
+
+    _FOO: typing.Optional[str] = None
+
+    @classmethod
+    def get_foo_var(cls) -> str:
+        assert cls._CONFIG
+        if cls._FOO is None:
+            cls._FOO = cls._CONFIG.get('foo')
+        return cls._FOO
