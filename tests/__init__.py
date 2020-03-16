2e2a3009eee80286af1784e580b8dc7b719abe2b
Charles Reid <chmreid@ucsc.edu>
2020-03-16 15:48:32 -0700

    move test config paths from decorators to __init__

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,13 @@
+import os
 import logging
 
 
 logger = logging.getLogger(__name__)
+
+
+test_dir = os.path.abspath(os.path.dirname(__file__))
+repo_root = os.path.join(test_dir, "..")
+config_dir = os.path.join(repo_root, "config")
+
+STANDALONE_CONFIG_PATH = os.path.join(config_dir, "config.example.json")
+INTEGRATION_CONFIG_PATH = os.path.join(config_dir, "config.integration.json")
