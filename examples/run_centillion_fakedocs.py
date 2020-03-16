3a197ac8a34f08fbd9123c8fba39289660334540
Chaz Reid <53452777+chmreid@users.noreply.github.com>
2020-03-16 14:15:03 -0700

    Remove attic/ and examples/ dirs (#28)

--- examples/run_centillion_fakedocs.py
+++
@@ -1,4 +0,0 @@
-import centillion
-
-app = centillion.webapp.get_flask_app(config_file='../config/config_centillion_fakedocs.py')
-app.run()
--- examples/run_centillion_gh.py
+++
@@ -1,4 +0,0 @@
-import centillion
-
-app = centillion.webapp.get_flask_app(config_file='../config/config_centillion_gh.py')
-app.run()
