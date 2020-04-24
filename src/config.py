874126cb8e14fd1202c2c64547e3be5955a330c6
Charles Reid <chmreid@ucsc.edu>
2020-04-23 17:27:38 -0700

    Config class: ensure creation of directories; add reset() method

--- src/config.py
+++ src/config.py
@@ -91,14 +91,24 @@ class Config(object):
     _DOCTYPES_NAMES_MAP: typing.Optional[typing.Dict[str, typing.List[str]]] = None
 
     @classmethod
+    def reset(cls) -> None:
+        cls._CONFIG_FILE = None
+        cls._CONFIG = None
+        cls._CENTILLION_ROOT = None
+        cls._CENTILLION_TMPDIR = None
+        cls._CENTILLION_INDEXDIR = None
+        cls._DOCTYPES_LIST = None
+        cls._DOCTYPES_NAMES_MAP = None
+
+    @classmethod
     def get_centillion_root(cls) -> str:
         if cls._CENTILLION_ROOT is None:
             if 'centillion_root' in cls._CONFIG:
                 cls._CENTILLION_ROOT = cls._CONFIG['centillion_root']
             else:
                 cls._CENTILLION_ROOT = Config.get_required_env_var('CENTILLION_ROOT')
-            if not os.path.isdir(cls._CENTILLION_ROOT):
-                raise CentillionConfigException(f"Error: Path {cls._CENTILLION_ROOT} is not a directory")
+        if not os.path.isdir(cls._CENTILLION_ROOT):
+            raise CentillionConfigException(f"Error: Path {cls._CENTILLION_ROOT} is not a directory")
         return cls._CENTILLION_ROOT
 
     @classmethod
@@ -110,18 +120,17 @@ class Config(object):
                 centillion_root = Config.get_centillion_root()
                 centillion_indexdir = os.path.join(centillion_root, 'index')
                 cls._CENTILLION_INDEXDIR = centillion_indexdir
-            if not os.path.isdir(centillion_indexdir):
-                subprocess.call(['mkdir', '-p', centillion_indexdir])
+        if not os.path.isdir(cls._CENTILLION_INDEXDIR):
+            subprocess.call(['mkdir', '-p', cls._CENTILLION_INDEXDIR])
         return cls._CENTILLION_INDEXDIR
 
     @classmethod
     def get_centillion_tmpdir(cls) -> str:
         if cls._CENTILLION_TMPDIR is None:
             centillion_root = Config.get_centillion_root()
-            tmp_dir = os.path.join(centillion_root, TMPDIR_NAME)
-            if not os.path.exists(tmp_dir):
-                subprocess.call(['mkdir', '-p', tmp_dir])
-            cls._CENTILLION_TMPDIR = tmp_dir
+            cls._CENTILLION_TMPDIR = os.path.join(centillion_root, TMPDIR_NAME)
+        if not os.path.exists(cls._CENTILLION_TMPDIR):
+            subprocess.call(['mkdir', '-p', cls._CENTILLION_TMPDIR])
         return cls._CENTILLION_TMPDIR
 
     @classmethod
