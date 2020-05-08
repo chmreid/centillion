d90224f787bd74281fbfebd0c661899953fb8e38
Charles Reid <chmreid@ucsc.edu>
2020-05-08 15:57:48 -0700

    add test for Search.delete_docs method

--- tests/test_search.py
+++ tests/test_search.py
@@ -87,7 +87,7 @@ class SearchTest(unittest.TestCase):
 
     def test_search_update_docs(self):
         """
-        Test the abiltiy to add, then update, a plain document
+        Test the ability to add, then update, a plain document
 
         This method tests these Search class methods:
         - add_doc
@@ -106,42 +106,91 @@ class SearchTest(unittest.TestCase):
                 s.add_doc(doc)
                 PlainDoctype.register_document(doc)
 
-            with self.subTest("Test update_docs method of Search class"):
-                # To update a document, we need to update the
-                # document in the "remote", which in this case
-                # is the PlainDoctype class's document_registry variable.
-                #
-                # Then we get the local and remote lists,
-                # and pass them to update_docs to update
-                # any documents that need updating.
+            # To update a document, we need to update the
+            # document in the "remote", which in this case
+            # is the PlainDoctype class's document_registry variable.
+            #
+            # Then we get the local and remote lists,
+            # and pass them to update_docs to update
+            # any documents that need updating.
 
-                # New dummy name for one of the documents
-                dummy_name = 'centillion-test-search-file-dingbat.dat'
+            # New dummy name for one of the documents
+            dummy_name = 'centillion-test-search-file-dingbat.dat'
 
-                # Doc id of document we will modify
-                doc_id = docs[0]['id']
+            # Doc id of document we will modify
+            doc_id = docs[0]['id']
 
-                # First, get the search index document and check it is the old version
-                # Adding the document above ensures the document will be in the search index.
-                search_ix_doc = s.get_by_id(doc_id)
-                self.assertNotEqual(search_ix_doc['name'], dummy_name)
+            # First, get the search index document and check it is the old version
+            # Adding the document above ensures the document will be in the search index.
+            search_ix_doc = s.get_by_id(doc_id)
+            self.assertNotEqual(search_ix_doc['name'], dummy_name)
 
-                # Modify the document in the "remote" (the PlainDoctype class)
-                doc = PlainDoctype.document_registry[doc_id]
-                doc['name'] = dummy_name
-                doc['modified_time'] = datetime.datetime.now().replace(microsecond=0) + datetime.timedelta(minutes=10)
+            # Modify the document in the "remote" (the PlainDoctype class)
+            doc = PlainDoctype.document_registry[doc_id]
+            doc['name'] = dummy_name
+            doc['modified_time'] = datetime.datetime.now().replace(microsecond=0) + datetime.timedelta(minutes=10)
 
-                # Re-register the updated document
-                PlainDoctype.register_document(doc)
+            # Re-register the updated document
+            PlainDoctype.register_document(doc)
 
-                # Call the update_docs method of the Search class to update the document in the search index
-                to_update = {doc_id}
-                remote_map = PlainDoctype.get_remote_map()
-                s.update_docs(to_update, doctype_instance, remote_map)
+            # Call the update_docs method of the Search class to update the document in the search index
+            to_update = {doc_id}
+            remote_map = PlainDoctype.get_remote_map()
+            s.update_docs(to_update, doctype_instance, remote_map)
 
-                # Get the search index document and ensure it was updated
-                search_ix_doc = s.get_by_id(doc_id)
-                self.assertEqual(search_ix_doc['name'], dummy_name)
+            # Get the search index document and ensure it was updated
+            search_ix_doc = s.get_by_id(doc_id)
+            self.assertEqual(search_ix_doc['name'], dummy_name)
+
+    def test_search_delete_docs(self):
+        """
+        Test the ability to add, then delete, a plain document
+
+        This method tests these Search class methods;
+        - add_doc
+        - delete_docs
+        """
+        doctype = "plain"
+        name = "centillion-test-search-add-doc"
+        doctype_instance = DoctypeRegistry.REGISTRY[doctype](name)
+        docs = get_plain_docs()
+        doc_ids_list = [d['id'] for d in docs]
+        doc_ids_set = {d['id'] for d in docs}
+
+        with TempCentillionConfig(get_plain_config()):
+            s = Search()
+
+            with self.subTest("Test delete_docs with list"):
+                for doc in docs:
+                    s.add_doc(doc)
+                    PlainDoctype.register_document(doc)
+
+                # Ensure added
+                for doc_id in doc_ids_list:
+                    self.assertNotEqual(s.get_by_id(doc_id), None)
+
+                # Test delete as list
+                s.delete_docs(doc_ids_list)
+
+                # Ensure deleted
+                for doc_id in doc_ids_list:
+                    self.assertEqual(s.get_by_id(doc_id), None)
+
+            with self.subTest("Test delete_docs with set"):
+                for doc in docs:
+                    s.add_doc(doc)
+                    PlainDoctype.register_document(doc)
+
+                # Ensure added
+                for doc_id in doc_ids_list:
+                    self.assertNotEqual(s.get_by_id(doc_id), None)
+
+                # Test delete as set
+                s.delete_docs(doc_ids_set)
+
+                # Ensure deleted
+                for doc_id in doc_ids_list:
+                    self.assertEqual(s.get_by_id(doc_id), None)
 
     def test_crud_plain_doc(self):
         """
