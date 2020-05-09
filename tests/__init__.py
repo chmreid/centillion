5c918fa6388607e88e1944bd3cbd2d01643ecd53
Charles Reid <chmreid@ucsc.edu>
2020-05-08 17:59:37 -0700

    make a temp dir variable for integration tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 test_dir = os.path.abspath(os.path.dirname(__file__))
 repo_root = os.path.join(test_dir, "..")
 config_dir = os.path.join(repo_root, "config")
+temp_dir = "/tmp/centillion"
 
 STANDALONE_CONFIG_PATH = os.path.join(config_dir, "config.example.json")
 INTEGRATION_CONFIG_PATH = os.path.join(config_dir, "config.integration.json")
