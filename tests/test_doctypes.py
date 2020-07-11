62bae1b2445966d1b0c569472036885d2b6964c7
Charles Reid <chmreid@ucsc.edu>
2020-07-11 15:26:36 -0700

    add doctype virtual methods test class

--- tests/test_doctypes.py
+++ tests/test_doctypes.py
@@ -7,8 +7,7 @@ from centillion.doctypes.doctype import Doctype
 
 logger = logging.getLogger(__name__)
 
-DOCTYPES = ["subclass_a", "subclass_b", "subclass_c"]
-
+DOCTYPES = ["subclass_a", "subclass_b", "subclass_c"]  # subclass_d is intentionally excluded
 
 class DoctypeRegistryTest(unittest.TestCase):
     """
@@ -28,11 +27,46 @@ class DoctypeRegistryTest(unittest.TestCase):
         pass
 
     def test_registry(self):
+        """Test that the new doctypes are in the Doctype registry, using the doctype attribute"""
         for doctype in DOCTYPES:
             self.assertIn(doctype, DoctypeRegistry.get_registry())
 
     def test_registry_noattr(self):
+        """Test that the new doctypes are in the Doctype registry, even if no doctype attribute is defined"""
         self.assertIn(self._TestDoctypeSubclassD.__name__, DoctypeRegistry.get_registry())
+
+    def test_registry(self):
+        """
+        Check that the doctype registry returns instances of the correct class
+        """
+        self.assertEqual(type(cls.get_registry()["subclass_a"]), self._TestDoctypeSubclassA)
+        self.assertEqual(type(cls.get_registry()["subclass_b"]), self._TestDoctypeSubclassB)
+        self.assertEqual(type(cls.get_registry()["subclass_c"]), self._TestDoctypeSubclassC)
+        self.assertEqual(type(cls.get_registry()[self._TestDoctypeSubclassD.__name__]), self._TestDoctypeSubclassD)
+
+
+class DoctypeVirtualMethodsTest(unittest.TestCase):
+    """
+    Check that if a derived doctype class does not have virtual methods defined,
+    centillion will raise a NotImplementedError.
+    """
+    class _TestDoctypeVirtualMethodsSubclassA(metaclass=DoctypeRegistry):
+        def __init__(self, *args, **kwargs):
+            pass
+
+    def test_virtual_method_init(self):
+        """
+        Check that a Doctype method that defines a constructor but no virtual methods will cause exceptions.
+        """
+        instanceA = self._TestDoctypeVirtualMethodsSubclassA()
+        with self.assertRaises(NotImplementedError):
+            instanceA.validate_credentials()
+        with self.assertRaises(NotImplementedError):
+            instanceA.get_remote_map()
+        with self.assertRaises(NotImplementedError):
+            instanceA.get_by_id()
+        with self.assertRaises(NotImplementedError):
+            instanceA.get_jinja_template()
 
 
 class DoctypeTest(unittest.TestCase):
@@ -70,6 +104,13 @@ class DoctypeTest(unittest.TestCase):
     def test_doctype(self):
         """Test the doctype class var is configured and accessible"""
         self.assertEqual(Doctype.doctype, "base")
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
